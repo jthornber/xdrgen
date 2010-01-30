57bdb43f1b93e5df9ab5c503366b337b8f3098bd
Joe Thornber <joe.thornber@gmail.com>
2010-01-30 14:28:03 +0000

    Promote a lot of the pretty printer functions to be top level, and add type decls

--- Data/XDR/PrettyPrinter.hs
+++ Data/XDR/PrettyPrinter.hs
@@ -20,61 +20,71 @@ braces ds = nest indent (lbrace <$> vcat ds) <$> rbrace
 
 ppXDR :: Specification -> String
 ppXDR = show . ppSpec
-    where
-      ppSpec (Specification defs) = vcat . punctuate linebreak . map ppDef $ defs
 
-      ppDef (DefTypedef td) = ppTypedef td
-      ppDef (DefConstant cd) = ppConstdef cd
 
-      ppTypedef (Typedef n ti) = text "typedef" <+> ppTypedefInternal n ti <> semi
+ppSpec :: Specification -> Doc        
+ppSpec (Specification defs) = vcat . punctuate linebreak . map ppDef $ defs
 
-      ppTypedefInternal n (DefSimple di) = ppDecl (Decl n di)
-      ppTypedefInternal n (DefEnum ed) = text "enum" <+> ppEnumDetail ed <+> text n
-      ppTypedefInternal n (DefStruct sd) = text "struct" <+> ppStructDetail sd <+> text n
-      ppTypedefInternal n (DefUnion ud) = text "union" <+> ppUnionDetail ud <+> text n
+ppDef :: Definition -> Doc
+ppDef (DefTypedef td) = ppTypedef td
+ppDef (DefConstant cd) = ppConstdef cd
 
-      ppEnumDetail (EnumDetail xs) = braces . punctuate comma . map ppEnumDef $ xs
-      ppEnumDef (n, c) = text n <+> text "=" <+> ppConstant c
+ppTypedef :: Typedef -> Doc
+ppTypedef (Typedef n ti) = text "typedef" <+> ppTypedefInternal n ti <> semi
 
-      ppStructDetail (StructDetail decls) = braces . map ((<> semi) . ppDecl) $ decls
+ppTypedefInternal :: String -> TypedefInternal -> Doc
+ppTypedefInternal n (DefSimple di) = ppDecl (Decl n di)
+ppTypedefInternal n (DefEnum ed) = text "enum" <+> ppEnumDetail ed <+> text n
+ppTypedefInternal n (DefStruct sd) = text "struct" <+> ppStructDetail sd <+> text n
+ppTypedefInternal n (DefUnion ud) = text "union" <+> ppUnionDetail ud <+> text n
 
-      ppUnionDetail (UnionDetail selector cases mDefault) =
-          vcat $ concat [ [ text "switch" <> parens (ppDecl selector) <+> lbrace ]
-                        , map ppCase cases
-                        , [ppDflt mDefault]
-                        , [rbrace]
-                        ]
+ppEnumDetail :: EnumDetail -> Doc
+ppEnumDetail (EnumDetail xs) = braces . punctuate comma . map (uncurry ppEnumDef) $ xs
 
-      ppCase (c, d) = nest indent (ppConstant c <> colon <$> ppDecl d <> semi)
-      ppDflt d = ppOptional (\d -> nest indent (text "default:" <$> ppDecl d)) d
+ppEnumDef :: String -> Constant -> Doc
+ppEnumDef n c = text n <+> text "=" <+> ppConstant c
 
-      ppConstdef (ConstantDef n c) = text "const" <+> text n <+> text "=" <+> ppConstant c <> semi
+ppStructDetail :: StructDetail -> Doc
+ppStructDetail (StructDetail decls) = braces . map ((<> semi) . ppDecl) $ decls
 
-      ppConstant (ConstLit n) = text . show $ n
+ppUnionDetail :: UnionDetail -> Doc
+ppUnionDetail (UnionDetail selector cases mDefault) =
+    vcat $ concat [ [ text "switch" <> parens (ppDecl selector) <+> lbrace ]
+                  , map ppCase cases
+                  , [ppDflt mDefault]
+                  , [rbrace]
+                  ]
 
-      ppDecl (Decl n (DeclSimple t)) = ppType t <+> text n
-      ppDecl (Decl n (DeclArray t c)) = ppType t <+> text n <> (brackets . ppConstant $ c)
-      ppDecl (Decl n (DeclVarArray t mc)) = ppType t <+> text n <+> ppVarSize mc
-      ppDecl (Decl n (DeclOpaque c)) = text "opaque" <+> text n <> (brackets . ppConstant $ c)
-      ppDecl (Decl n (DeclVarOpaque mc)) = text "opaque" <+> text n <> ppVarSize mc
-      ppDecl (Decl n (DeclString mc)) = text "string" <+> text n <> ppVarSize mc
-      ppDecl (Decl n (DeclPointer t)) = ppType t <> text "*" <+> text n
-      ppDecl DeclVoid = text "void"
+ppCase (c, d) = nest indent (ppConstant c <> colon <$> ppDecl d <> semi)
+ppDflt d = ppOptional (\d -> nest indent (text "default:" <$> ppDecl d)) d
 
-      ppVarSize :: Maybe Constant -> Doc
-      ppVarSize = angles . ppOptional ppConstant
+ppConstdef (ConstantDef n c) = text "const" <+> text n <+> text "=" <+> ppConstant c <> semi
 
-      ppType TInt = text "int"
-      ppType TUInt = text "unsigned int"
-      ppType THyper = text "hyper"
-      ppType TUHyper = text "unsigned hyper"
-      ppType TFloat = text "float"
-      ppType TDouble = text "double"
-      ppType TQuad = text "quad"
-      ppType TBool = text "bool"
-      ppType (TEnum ed) = text "enum" <+> ppEnumDetail ed
-      ppType (TStruct sd) = text "struct" <+> ppStructDetail sd
-      ppType (TUnion ud) = text "union" <+> ppUnionDetail ud
-      ppType (TTypedef n) = text n
+ppConstant (ConstLit n) = text . show $ n
+
+ppDecl (Decl n (DeclSimple t)) = ppType t <+> text n
+ppDecl (Decl n (DeclArray t c)) = ppType t <+> text n <> (brackets . ppConstant $ c)
+ppDecl (Decl n (DeclVarArray t mc)) = ppType t <+> text n <+> ppVarSize mc
+ppDecl (Decl n (DeclOpaque c)) = text "opaque" <+> text n <> (brackets . ppConstant $ c)
+ppDecl (Decl n (DeclVarOpaque mc)) = text "opaque" <+> text n <> ppVarSize mc
+ppDecl (Decl n (DeclString mc)) = text "string" <+> text n <> ppVarSize mc
+ppDecl (Decl n (DeclPointer t)) = ppType t <> text "*" <+> text n
+ppDecl DeclVoid = text "void"
+
+ppVarSize :: Maybe Constant -> Doc
+ppVarSize = angles . ppOptional ppConstant
+
+ppType TInt = text "int"
+ppType TUInt = text "unsigned int"
+ppType THyper = text "hyper"
+ppType TUHyper = text "unsigned hyper"
+ppType TFloat = text "float"
+ppType TDouble = text "double"
+ppType TQuad = text "quad"
+ppType TBool = text "bool"
+ppType (TEnum ed) = text "enum" <+> ppEnumDetail ed
+ppType (TStruct sd) = text "struct" <+> ppStructDetail sd
+ppType (TUnion ud) = text "union" <+> ppUnionDetail ud
+ppType (TTypedef n) = text n
 
 ----------------------------------------------------------------
