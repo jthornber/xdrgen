312c83b41bd63fb2e1d009187e4bac0f78a0ac08
Joe Thornber <joe.thornber@gmail.com>
2010-01-30 15:42:16 +0000

    xdrgen takes a single file arg, or reads from stdin

--- XDRCompile.hs
+++ XDRCompile.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Control.Applicative
+import Control.Monad
 import qualified Data.ByteString as B
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
+import System.IO
 
 import Data.XDR.Parser
 import Data.XDR.PrettyPrinter
@@ -20,11 +22,26 @@ import Data.XDR.PrettyPrintC
 -- options =
 --     [ Option ['I'] ["include"] (ReqArg Include "INCLUDE DIR") "directory to search for XDR source files"
 
+showResult (Left errs) = hPutStrLn stderr (unlines . map show $ errs)
+showResult (Right spec) = putStrLn $ ppXDR spec
+
+doFile f = parseFile [] f >>= showResult
+
+doStdin = do
+  txt <- B.getContents
+  let ast = parseString [] txt "<stdin>"
+  showResult ast
+
 main :: IO ()
 main = do
-  txt <- B.getContents
-  putStrLn $ case parseString [] txt "" of
-               Left errs  -> unlines . map show $ errs
-               Right spec -> ppXDR $ spec
+  args <- getArgs
+  case args of
+    [] -> doStdin
+    [f] -> doFile f
+    _ -> usage
+
+usage = do
+  putStrLn "usage: xdrgen <file>"
+  exitFailure
 
 ----------------------------------------------------------------
