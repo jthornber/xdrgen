4eb767a85af7427585d2a3ad797fed796fe85237
Mark Aylett <mark.aylett@gmail.com>
2010-05-23 08:01:44 +0100

    Java refactoring.

--- Data/XDR/PrettyPrintJava.hs
+++ Data/XDR/PrettyPrintJava.hs
@@ -11,7 +11,7 @@ import Data.Maybe
 import Data.XDR.AST
 import Data.XDR.PPKeywords
 import Data.XDR.PPUtils
-import Text.PrettyPrint.Leijen as PP hiding (semiBraces, braces, indent)
+import Text.PrettyPrint.Leijen as PP hiding (braces, indent)
 
 -- | Bird.
 
@@ -29,11 +29,8 @@ indent = 4
 lrparen :: Doc
 lrparen = lparen <> rparen
 
-braces :: [Doc] -> Doc
-braces ds = nest indent (lbrace <$> vcat ds) <$> rbrace
-
-semiBraces :: [Doc] -> Doc
-semiBraces = braces . map (<> semi)
+braces :: Doc -> Doc
+braces d = nest indent (lbrace <$> d) <$> rbrace
 
 maybePush :: Maybe a -> [a] -> [a]
 maybePush =
@@ -173,9 +170,10 @@ lookupType m n (TUnion (UnionDetail sel _ _)) =
 lookupType m _ (TTypedef n) =
     JType td ud cp
   where
-    td = maybe (text $ typeName n) jType jt
-    ud = maybe (text $ typeName n) jUnbox jt
+    td = maybe td' jType jt
+    ud = maybe td' jUnbox jt
     cp = maybe ppConstPrim jConstPrim jt
+    td' = text $ typeName n
     jt = lookupPair m `fmap` M.lookup n m
 
 kByteBuffer = text "java.nio.ByteBuffer"
@@ -211,7 +209,7 @@ ppCodecType _ _ TQuadruple = error "not supported"
 ppCodecType _ _ TBool = text "XdrBool.CODEC"
 ppCodecType _ _ (TEnum _) = text "XdrInt.CODEC"
 ppCodecType _ n (TStruct _) = text ("Xdr" ++ typeName n ++ ".CODEC")
-ppCodecType m n (TUnion (UnionDetail sel _ mDef)) =
+ppCodecType m _ (TUnion (UnionDetail sel _ mDef)) =
     text "XdrUnion.newCodec"
              <> tupled ((fromMaybe (text "XdrInt.CODEC") sd)
                         : text "CASES" : maybeToList dd)
@@ -239,7 +237,7 @@ ppMaybePackage (Module elems) =
 
 ppImports :: [Module] -> Doc
 ppImports =
-    (text "import org.openxdr.*" <> semi <$>) . vcat . map ppImport
+    (kImport <+> text "org.openxdr.*" <> semi <$>) . vcat . map ppImport
 
 ppImport :: Module -> Doc
 ppImport m@(Module elems) =
@@ -249,9 +247,9 @@ ppImport m@(Module elems) =
 
 ppClass :: String -> [Doc] -> Doc
 ppClass n =
-    (lead <+>) . braces . (ppPrivateCons n :)
+    (head <+>) . braces . vcat . (ppPrivateCons n :)
   where
-    lead = kClass <+> text n
+    head = kClass <+> text n
 
 ppPrivateCons :: String -> Doc
 ppPrivateCons n =
@@ -288,22 +286,11 @@ ppStaticCodec n t d =
     decl = kStatic <+> kFinal <+> text "Codec"
            <> langle <> t <> rangle <+> text n
 
-ppIface :: String -> [Doc] -> Doc
+ppIface :: String -> [(String, JType)] -> Doc
 ppIface n =
-    (lead <+>) . braces
+    (head <+>) . braces . vcat . map ppGetterDecl
   where
-    lead = kInterface <+> text tn
-    tn = typeName n
-
-ppEnumBody :: [(String, ConstPrim)] -> [Doc]
-ppEnumBody =
-    map f
-  where
-    f (n, c) = ppConstDef n (text . show $ evalConstPrim c)
-
-ppIfaceBody :: [(String, JType)] -> [Doc]
-ppIfaceBody =
-    map ppGetterDecl
+    head = kInterface <+> text n
 
 ppGetterName :: String -> Doc
 ppGetterName n =
@@ -313,40 +300,75 @@ ppGetterDecl :: (String, JType) -> Doc
 ppGetterDecl (n, jt) =
     jUnbox jt <+> ppGetterName n <> lrparen <> semi
 
-ppAnonCodec :: String -> [Doc] -> Doc
-ppAnonCodec n =
-    (nest indent lead <+>) . (<> semi) . braces
+ppFactory :: String -> [(String, JType)] -> Doc
+ppFactory n jts =
+    (head <+>) . braces $ ppInstance n jts
   where
-    lead = kPublic <+> kStatic <+> kFinal <+> ct <+> text "CODEC" <+> equals
+    head = kStatic <+> kFinal <+> text n <+> text ("new" ++ n)
+           <> tupled (ppFactoryParams jts)
+
+ppInstance :: String -> [(String, JType)] -> Doc
+ppInstance n =
+    (<> semi) . (head <+>) . braces . vcat . ppGetterImpls
+  where
+    head = kReturn <+> kNew <+> text n <> lrparen
+
+ppGetterImpls :: [(String, JType)] -> [Doc]
+ppGetterImpls =
+    map ppGetterImpl
+
+ppGetterImpl :: (String, JType) -> Doc
+ppGetterImpl (n, jt) =
+    kPublic <+> kFinal <+> jUnbox jt
+                <+> ppGetterName n <> lrparen
+                <+> braces body
+  where
+    body = kReturn <+> text (cn ++ "_") <> semi
+    cn = camelCase n
+
+ppFactoryParams :: [(String, JType)] -> [Doc]
+ppFactoryParams =
+    map ppFactoryParam
+
+ppFactoryParam :: (String, JType) -> Doc
+ppFactoryParam (n, jt) =
+    kFinal <+> jUnbox jt <+> text (cn ++ "_")
+  where
+    cn = camelCase n
+
+ppFactoryCall :: String -> [(String, JType)] -> Doc
+ppFactoryCall n =
+    (head <>) . (<> semi) . tupled . ppFactoryArgs
+  where
+    head = kReturn <+> text ("new" ++ tn)
+    tn = typeName n
+
+ppFactoryArgs :: [(String, JType)] -> [Doc]
+ppFactoryArgs =
+    map ppFactoryArg
+
+ppFactoryArg :: (String, JType) -> Doc
+ppFactoryArg (n, _) =
+    text (cn ++ "_")
+  where
+    cn = camelCase n
+
+ppAnonCodec :: String -> [(String, JType)] -> Doc
+ppAnonCodec n jts =
+    nest indent (head <+> braces body) <> semi
+  where
+    head = kPublic <+> kStatic <+> kFinal <+> ct <+> text "CODEC" <+> equals
            </> kNew <+> ct <> lrparen
+    body = ppEncode n jts <$> ppDecode n jts
     ct = text "Codec" <> langle <> text tn <> rangle
     tn = typeName n
 
-ppCodecDecls :: DeclMap -> [DeclPair] -> [Doc]
-ppCodecDecls m =
-    map (ppCodecDecl m)
-
-ppCodecDecl :: DeclMap -> DeclPair -> Doc
-ppCodecDecl m p@(DeclPair n d) =
-    ppPrivateCodec n td (ppCodecPair m p)
-  where
-    td = jType $ lookupPair m p
-
-ppEncode :: String -> [Doc] -> Doc
+ppEncode :: String -> [(String, JType)] -> Doc
 ppEncode n =
-    (nest indent lead <+>) . semiBraces
+    (nest indent head <+>) . braces . vcat . ppEncodeVars
   where
-    lead = kPublic <+> kFinal <+> kVoid <+> text "encode"
+    head = kPublic <+> kFinal <+> kVoid <+> text "encode"
            <> tupled [kByteBuffer <+> text "buf", text (tn ++ " val")]
-           </> kThrows <+> kCharacterCodingException
-    tn = typeName n
-
-ppDecode :: String -> [Doc] -> Doc
-ppDecode n =
-    (nest indent lead <+>) . semiBraces
-  where
-    lead = kPublic <+> kFinal <+> text tn <+> text "decode"
-           <> tupled [kByteBuffer <+> text "buf"]
            </> kThrows <+> kCharacterCodingException
     tn = typeName n
 
@@ -357,10 +379,23 @@ ppEncodeVars =
 ppEncodeVar :: (String, JType) -> Doc
 ppEncodeVar (n, jt) =
     text un <> text "_CODEC.encode"
-             <> tupled [text "buf", text ("val.get" ++ tn) <> lrparen]
+             <> tupled [text "buf", text ("val.get" ++ tn) <> lrparen] <> semi
   where
     tn = typeName n
     un = upperName n
+
+ppDecode :: String -> [(String, JType)] -> Doc
+ppDecode n =
+    (nest indent head <+>) . braces . vcat . ppDecodeBody n
+  where
+    head = kPublic <+> kFinal <+> text tn <+> text "decode"
+           <> tupled [kByteBuffer <+> text "buf"]
+           </> kThrows <+> kCharacterCodingException
+    tn = typeName n
+
+ppDecodeBody :: String -> [(String, JType)] -> [Doc]
+ppDecodeBody n jts =
+    ppDecodeVars jts ++ [ppFactoryCall n jts]
 
 ppDecodeVars :: [(String, JType)] -> [Doc]
 ppDecodeVars =
@@ -369,30 +404,21 @@ ppDecodeVars =
 ppDecodeVar :: (String, JType) -> Doc
 ppDecodeVar (n, jt) =
     kFinal <+> jUnbox jt <+> text (cn ++ "_") <+> equals
-               <+> text un <> text "_CODEC.decode" <> tupled [text "buf"]
+               <+> text un <> text "_CODEC.decode"
+               <> tupled [text "buf"] <> semi
   where
     cn = camelCase n
     un = upperName n
 
-ppInner :: String -> [Doc] -> Doc
-ppInner n =
-    (lead <+>) . braces
-  where
-    lead = kReturn <+> kNew <+> text tn <> lrparen
-    tn = typeName n
+ppCodecDecls :: DeclMap -> [DeclPair] -> [Doc]
+ppCodecDecls m =
+    map (ppCodecDecl m)
 
-ppInnerBody :: [(String, JType)] -> [Doc]
-ppInnerBody =
-    map ppGetterImpl
-
-ppGetterImpl :: (String, JType) -> Doc
-ppGetterImpl (n, jt) =
-    kPublic <+> kFinal <+> jUnbox jt
-                <+> ppGetterName n <> lrparen
-                <+> nest indent (lbrace <$> body) <$> rbrace
+ppCodecDecl :: DeclMap -> DeclPair -> Doc
+ppCodecDecl m p@(DeclPair n d) =
+    ppPrivateCodec n td (ppCodecPair m p)
   where
-    body = kReturn <+> text (cn ++ "_") <> semi
-    cn = camelCase n
+    td = jType $ lookupPair m p
 
 ppJava :: Specification -> String
 ppJava spec =
@@ -435,28 +461,30 @@ ppJava spec =
     -- are not enclosed in a separate class or interface.
 
     ppEnumDetail n (EnumDetail pairs) =
-        vcat $ ppEnumBody pairs
+        vcat $ map f pairs
+      where
+        f (n, c) = ppConstDef n (text . show $ evalConstPrim c)
 
     ppStructDetail m n (StructDetail decls) =
-        kPublic <+> ppIface n (ppIfaceBody jts)
+        kPublic <+> ppIface tn jts
+                    <$> kPublic <+> ppFactory tn jts
                     <$> kPublic <+> kStatic <+> kFinal
-                    <+> ppClass ("Xdr" ++ typeName n)
-                            (ppAnonCodec n ([enc, dec]) : ppCodecDecls m dps)
+                    <+> ppClass ("Xdr" ++ tn) (anc : ppCodecDecls m dps)
       where
-        enc = ppEncode n (ppEncodeVars jts)
-        dec = ppDecode n (ppDecodeVars jts
-                          ++ [ppInner n (ppInnerBody jts)])
+        anc = ppAnonCodec n jts
         jts = map (pair (declName, lookupPair m)) dps
         dps = declsToPairs decls []
+        tn = typeName n
 
     ppUnionCases m (UnionDetail sel cases mDef) =
-        nest indent lead <> semi
+        nest indent head <> semi
       where
-        lead = kPrivate <+> kStatic <+> kFinal <+> text "Map" <> langle
-               <> (maybe (text "Integer") jType jt) <> char ',' <+> text "Codec" <> langle
-               <> char '?' <> rangle <> rangle <+> text "CASES" <+> equals
-               </> text "XdrUnion.newCases"
-               <> tupled (ppUnionPairs m (maybe ppConstPrim jConstPrim jt) cases)
+        head = kPrivate <+> kStatic <+> kFinal <+> text "Map" <> langle
+               <> (maybe (text "Integer") jType jt) <> char ','
+               <+> text "Codec" <> langle <> char '?' <> rangle <> rangle
+               <+> text "CASES" <+> equals </> text "XdrUnion.newCases"
+               <> tupled (ppUnionPairs m
+                          (maybe ppConstPrim jConstPrim jt) cases)
         jt = lookupPair m `fmap` sp
         sp = declToPair sel
 
